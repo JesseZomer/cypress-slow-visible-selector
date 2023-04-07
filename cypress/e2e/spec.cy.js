75fe32f957b907235b33fb390ad6cb47c53c1261
Jesse Zomer <jessezomer@hotmail.com>
2023-04-07 14:42:19 +0200

    added example tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,28 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("page", () => {
+  it("is not slow", () => {
+    cy.visit(
+      "https://developer.mozilla.org/en-US/docs/Web/CSS/Attribute_selectors"
+    );
+    cy.get('[aria-labelledby="examples"]').should("be.visible");
+  });
+
+  it("is also not slow", () => {
+    cy.visit(
+      "https://developer.mozilla.org/en-US/docs/Web/CSS/Attribute_selectors"
+    );
+    cy.get('[aria-labelledby="examples"]')
+      .filter(":visible")
+      .should("be.visible");
+  });
+
+  it("is slow and runner gets stuck for little while", () => {
+    cy.visit(
+      "https://developer.mozilla.org/en-US/docs/Web/CSS/Attribute_selectors"
+    );
+    cy.get('[aria-labelledby="see_also"]:visible').should(
+      "include.text",
+      "See also"
+    );
+  });
+});
